17ee82eec2afcb1876bd479f901087c73f6e8fe2
Neeraj Gupta <gneeraj970@gmail.com>
2024-11-08 21:06:45 +0530

    Added pipeline configuration for ekartApplication with about file.

---
+++ 1-EkartApplication-Java/trivyInstallation.sh
@@ -0,0 +1,6 @@
+apt install -y gnupg curl
+curl -fsSL https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor -o /usr/share/keyrings/trivy.gpg
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/trivy.list
+apt update
+apt install -y trivy
+trivy --version
